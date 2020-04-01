078df816133cfaf63ccd4d4d140bdfa75562db68
Thummanoon hwancaheam <42062759+artborza@users.noreply.github.com>
2020-04-01 08:42:34 +0700

    Update 4/1/2020

--- JavaTest/src/MergeSort.java
+++
@@ -1,6 +0,0 @@
-public class MergeSort extends Strategy{
-    public static void main(String args[]) {
-        MergeSort bubble = new MergeSort();
-        bubble.setSort("MergeSort");
-    }
-}
--- JavaTest/src/bubbleSort.java
+++
@@ -1,7 +0,0 @@
-public class bubbleSort extends Strategy {
-    public static void main(String args[]) {
-        bubbleSort bubble = new bubbleSort();
-        bubble.setSort("bubbleSort");
-    }
-
-}
